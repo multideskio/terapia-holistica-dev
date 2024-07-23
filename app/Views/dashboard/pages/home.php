a2d0d78b364c15ed9322554baf26601dece38abc
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-07-23 16:33:01 -0300

    Update home.php

--- app/Views/dashboard/pages/home.php
+++ app/Views/dashboard/pages/home.php
@@ -1,10 +1,7 @@
 <?= $this->extend('dashboard/template') ?>
-
 <?php $this->section('page') ?>
-
 <p>Dados de sessão:</p>
 <pre>
 <?php print_r(session('data')) ?>
 </pre>
-
 <?php $this->endSection(); ?>
