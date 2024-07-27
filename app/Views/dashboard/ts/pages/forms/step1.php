1ce331c6e686fcf5f9775f70e9d6bf5dbef806d2
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-07-27 17:51:01 -0300

    Update step1.php

--- app/Views/dashboard/ts/pages/forms/step1.php
+++ app/Views/dashboard/ts/pages/forms/step1.php
@@ -19,8 +19,33 @@
                 <input type="hidden" name="idPatient" id="idPatient">
             </div>
             <div class="text-end mt-3">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalId">Atualizar paciente</button>
                 <button type="button" class="btn btn-primary next-btn" data-next="#collapseTwo">Próximo</button>
             </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+<div class="modal fade" id="modalId" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalTitleId">
+                    Atualizar paciente
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <?= form_open('', 'class=""') ?>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+            </div>
+            </form>
         </div>
     </div>
 </div>
