6022fbd4851614abf420b862be72a85b6cc69c3f
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-06 21:43:14 -0300

    TP - TS

--- app/Views/dashboard/tp/pages/dashboard.php
+++ app/Views/dashboard/tp/pages/dashboard.php
@@ -3,7 +3,17 @@
 <link rel="stylesheet" href="/assets/css/custom/anamnese.css?v=<?= time() ?>">
 <?php $this->endSection(); ?>
 <?php $this->section('page') ?>
-
+<div class="row mb-3 pb-1">
+    <div class="col-12">
+        <div class="d-flex align-items-lg-center flex-lg-row flex-column">
+            <div class="flex-grow-1">
+                <h1 class="fw-bolder">DASHBOARD</h1>
+            </div>
+        </div><!-- end card header -->
+    </div>
+    <!--end col-->
+</div>
+<!--end row-->
 
 
 
--- app/Views/dashboard/ts/pages/timeline.php
+++ app/Views/dashboard/ts/pages/timeline.php
@@ -3,6 +3,17 @@
 
 <?php $this->endSection(); ?>
 <?php $this->section('page') ?>
+<div class="row mb-3 pb-1">
+    <div class="col-12">
+        <div class="d-flex align-items-lg-center flex-lg-row flex-column">
+            <div class="flex-grow-1">
+                <h1 class="fw-bolder">DASHBOARD</h1>
+            </div>
+        </div><!-- end card header -->
+    </div>
+    <!--end col-->
+</div>
+<!--end row-->
 <div class="row">
     <div class="col-lg-12">
         <div>
