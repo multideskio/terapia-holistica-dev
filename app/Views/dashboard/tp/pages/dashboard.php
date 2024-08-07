71892c32bcb28baeac8c0ae729f1117a49ecdc4f
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-06 23:26:53 -0300

    Update dashboard.php

--- app/Views/dashboard/tp/pages/dashboard.php
+++ app/Views/dashboard/tp/pages/dashboard.php
@@ -3,11 +3,13 @@
 <link rel="stylesheet" href="/assets/css/custom/anamnese.css?v=<?= time() ?>">
 <?php $this->endSection(); ?>
 <?php $this->section('page') ?>
-<div class="row">
-    <div class="col-12">
-        <h1 class="fw-bolder p-0 m-0">DASHBOARD</h1>
-    </div>
+
+
+<div class="col-xxl-12">
+    <h1 class="fw-bolder p-0 m-0">DASHBOARD</h1>
 </div>
+
+
 <?php $this->endSection(); ?>
 <?php $this->section('js'); ?>
 
