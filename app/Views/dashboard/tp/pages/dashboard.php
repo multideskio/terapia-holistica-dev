1259c7226c192e038f7372eb31ff2a8ca500df2e
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-10 17:27:22 -0300

    Update dashboard.php

--- app/Views/dashboard/tp/pages/dashboard.php
+++ app/Views/dashboard/tp/pages/dashboard.php
@@ -21,172 +21,12 @@
 <?php $this->endSection(); ?>
 <?php $this->section('page') ?>
 <div class="col-xxl-12">
-    <h1 class="fw-bolder p-0 m-0">DASHBOARD</h1>
+    <h1 class="fw-bolder p-0">DASHBOARD</h1>
 </div>
 
-
 <div class="row">
-    <div class="col-xl-3 col-md-6">
-        <!-- card -->
-        <div class="card card-animate">
-            <div class="card-body">
-                <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 overflow-hidden">
-                        <p class="text-uppercase fw-medium text-muted text-truncate mb-0"> Resumo</p>
-                    </div>
-                    <div class="flex-shrink-0">
-                        <h5 class="text-success fs-14 mb-0">
-                            <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +16.24 %
-                        </h5>
-                    </div>
-                </div>
-                <div class="d-flex align-items-end justify-content-between mt-4">
-                    <div>
-                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="100">0</span></h4>
-                        <span class="text-muted">Atendimentos</span>
-                    </div>
-                    <div class="avatar-sm flex-shrink-0">
-                        <span class="avatar-title bg-success-subtle rounded fs-3">
-                            <i class="bx bx-dollar-circle text-success"></i>
-                        </span>
-                    </div>
-                </div>
-            </div><!-- end card body -->
-        </div><!-- end card -->
-    </div><!-- end col -->
-
-    <div class="col-xl-3 col-md-6">
-        <!-- card -->
-        <div class="card card-animate">
-            <div class="card-body">
-                <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 overflow-hidden">
-                        <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Atendimentos</p>
-                    </div>
-                    <div class="flex-shrink-0">
-                        <h5 class="text-danger fs-14 mb-0">
-                            <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
-                        </h5>
-                    </div>
-                </div>
-                <div class="d-flex align-items-end justify-content-between mt-4">
-                    <div>
-                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="36894">0</span></h4>
-                        <a href="" class="text-decoration-underline">View all orders</a>
-                    </div>
-                    <div class="avatar-sm flex-shrink-0">
-                        <span class="avatar-title bg-info-subtle rounded fs-3">
-                            <i class="bx bx-shopping-bag text-info"></i>
-                        </span>
-                    </div>
-                </div>
-            </div><!-- end card body -->
-        </div><!-- end card -->
-    </div><!-- end col -->
-
-    <div class="col-xl-3 col-md-6">
-        <!-- card -->
-        <div class="card card-animate">
-            <div class="card-body">
-                <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 overflow-hidden">
-                        <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Anamneses</p>
-                    </div>
-                    <div class="flex-shrink-0">
-                        <h5 class="text-success fs-14 mb-0">
-                            <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +29.08 %
-                        </h5>
-                    </div>
-                </div>
-                <div class="d-flex align-items-end justify-content-between mt-4">
-                    <div>
-                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="183.35">0</span>M </h4>
-                        <a href="" class="text-decoration-underline">See details</a>
-                    </div>
-                    <div class="avatar-sm flex-shrink-0">
-                        <span class="avatar-title bg-warning-subtle rounded fs-3">
-                            <i class="bx bx-user-circle text-warning"></i>
-                        </span>
-                    </div>
-                </div>
-            </div><!-- end card body -->
-        </div><!-- end card -->
-    </div><!-- end col -->
-
-    <div class="col-xl-3 col-md-6">
-        <!-- card -->
-        <div class="card card-animate">
-            <div class="card-body">
-                <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 overflow-hidden">
-                        <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Primeira consulta</p>
-                    </div>
-                    <div class="flex-shrink-0">
-                        <h5 class="text-muted fs-14 mb-0">
-                            +0.00 %
-                        </h5>
-                    </div>
-                </div>
-                <div class="d-flex align-items-end justify-content-between mt-4">
-                    <div>
-                        <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="165.89">0</span>k </h4>
-                        <a href="" class="text-decoration-underline">Withdraw money</a>
-                    </div>
-                    <div class="avatar-sm flex-shrink-0">
-                        <span class="avatar-title bg-primary-subtle rounded fs-3">
-                            <i class="bx bx-wallet text-primary"></i>
-                        </span>
-                    </div>
-                </div>
-            </div><!-- end card body -->
-        </div><!-- end card -->
-    </div><!-- end col -->
-</div> <!-- end row-->
-
-
-<div class="row">
-    <!-- Card 1 -->
     <div class="col-md-3">
-        <div class="card bg-light mb-3">
-            <div class="card-body">
-
-                <div class="text-center">
-                    <h1 class="card-title text-primary fw-bold">21</h1>
-                    <p class="card-subtitle mb-2">Atendimentos</p>
-                    <p class="text-muted">Finalizados: 13</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Card 2 -->
-    <div class="col-md-3">
-        <div class="card bg-light mb-3">
-            <div class="card-body text-center">
-
-                <div class="text-center">
-                    <h1 class="card-title text-danger fw-bold">17</h1>
-                    <p class="card-subtitle mb-2">Atendimentos</p>
-                    <p class="text-muted">Última semana: 9</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Card 3 -->
-    <div class="col-md-3">
-        <div class="card bg-light mb-3">
-            <div class="card-body text-center">
-
-                <div class="text-center">
-                    <h1 class="card-title text-warning fw-bold">24</h1>
-                    <p class="card-subtitle mb-2">Anamneses</p>
-                    <p class="text-muted">Último mês: 19</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Card 4 -->
-    <div class="col-md-3">
-        <div class="card bg-light mb-3">
+        <div class="card bg-light mb-3 card-animate">
             <div class="card-body text-center">
 
                 <div class="float-end">
@@ -205,7 +45,112 @@
 
                 <div class="d-flex" id="title">
                     <div class="flex-grow-1 ps-3">
-                        <h5 class="text-muted text-uppercase fs-13 mb-0">Total Revenue</h5>
+                        <h5 class="text-muted text-uppercase fs-13 mb-0">Resumo</h5>
+                    </div>
+                </div>
+
+                <div class="mt-4 pt-1">
+                    <h4 class="fs-1 fw-bold ff-secondary text-info mb-0">
+                        <span class="counter-value" data-target="100"></span>
+                    </h4>
+                    <p class="card-subtitle mb-3">Atendimentos</p>
+                    <p class="text-muted">Finalizados: <b>100</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card bg-light mb-3 card-animate">
+            <div class="card-body text-center">
+
+                <div class="float-end">
+                    <div class="dropdown card-header-dropdown">
+                        <a class="text-reset dropdown-btn" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="text-muted fs-18"><i class="mdi mdi-dots-vertical align-middle"></i></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="#">Today</a>
+                            <a class="dropdown-item" href="#">Last Week</a>
+                            <a class="dropdown-item" href="#">Last Month</a>
+                            <a class="dropdown-item" href="#">Current Year</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex" id="title">
+                    <div class="flex-grow-1 ps-3">
+                        <h5 class="text-muted text-uppercase fs-13 mb-0">Cancelados</h5>
+                    </div>
+                </div>
+
+                <div class="mt-4 pt-1">
+                    <h4 class="fs-1 fw-bold ff-secondary text-danger mb-0">
+                        <span class="counter-value" data-target="100"></span>
+                    </h4>
+                    <p class="card-subtitle mb-3">Atendimentos</p>
+                    <p class="text-muted">Última semana: <b>100</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-md-3">
+        <div class="card bg-light mb-3 card-animate">
+            <div class="card-body text-center">
+
+                <div class="float-end">
+                    <div class="dropdown card-header-dropdown">
+                        <a class="text-reset dropdown-btn" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="text-muted fs-18"><i class="mdi mdi-dots-vertical align-middle"></i></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="#">Today</a>
+                            <a class="dropdown-item" href="#">Last Week</a>
+                            <a class="dropdown-item" href="#">Last Month</a>
+                            <a class="dropdown-item" href="#">Current Year</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex" id="title">
+                    <div class="flex-grow-1 ps-3">
+                        <h5 class="text-muted text-uppercase fs-13 mb-0">Anamneses</h5>
+                    </div>
+                </div>
+
+                <div class="mt-4 pt-1">
+                    <h4 class="fs-1 fw-bold ff-secondary text-warning mb-0">
+                        <span class="counter-value" data-target="100"></span>
+                    </h4>
+                    <p class="card-subtitle mb-3">Anamneses</p>
+                    <p class="text-muted">Último mês: <b>100</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-md-3">
+        <div class="card bg-light mb-3 card-animate">
+            <div class="card-body text-center">
+                <div class="float-end">
+                    <div class="dropdown card-header-dropdown">
+                        <a class="text-reset dropdown-btn" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="text-muted fs-18"><i class="mdi mdi-dots-vertical align-middle"></i></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="#">Today</a>
+                            <a class="dropdown-item" href="#">Last Week</a>
+                            <a class="dropdown-item" href="#">Last Month</a>
+                            <a class="dropdown-item" href="#">Current Year</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex" id="title">
+                    <div class="flex-grow-1 ps-3">
+                        <h5 class="text-muted text-uppercase fs-13 mb-0">Primeira consulta</h5>
                     </div>
                 </div>
 
@@ -213,8 +158,8 @@
                     <h4 class="fs-1 fw-bold ff-secondary text-success mb-0">
                         <span class="counter-value" data-target="100"></span>
                     </h4>
-                    <p class="card-subtitle mb-2">Consultas</p>
-                    <p class="text-muted">Agendados: 16</p>
+                    <p class="card-subtitle mb-3">Consultas</p>
+                    <p class="text-muted">Agendados: <b>10</b></p>
                 </div>
             </div>
         </div>
@@ -222,7 +167,97 @@
 </div>
 
 
+<div class="row">
+    <div class="col-lg-8">
+        <dic class="card">
+            <div class="card-body">
+                <div class="chart">
+
+                </div>
+            </div>
+        </dic>
+    </div>
+</div>
+
+
 <?php $this->endSection(); ?>
 <?php $this->section('js'); ?>
+<!-- linecharts init -->
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
+<script>
+    var options = {
+        series: [{
+                name: "High - 2013",
+                data: [28, 29, 33, 36, 32, 32, 33]
+            },
+            {
+                name: "Low - 2013",
+                data: [12, 11, 14, 18, 17, 13, 13]
+            }
+        ],
+        chart: {
+            height: 350,
+            type: 'line',
+            dropShadow: {
+                enabled: true,
+                color: '#000',
+                top: 18,
+                left: 7,
+                blur: 10,
+                opacity: 0.2
+            },
+            zoom: {
+                enabled: false
+            },
+            toolbar: {
+                show: false
+            }
+        },
+        colors: ['#77B6EA', '#545454'],
+        dataLabels: {
+            enabled: true,
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        title: {
+            text: 'Average High & Low Temperature',
+            align: 'left'
+        },
+        grid: {
+            borderColor: '#e7e7e7',
+            row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+            },
+        },
+        markers: {
+            size: 1
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            title: {
+                text: 'Month'
+            }
+        },
+        yaxis: {
+            title: {
+                text: 'Temperature'
+            },
+            min: 5,
+            max: 40
+        },
+        legend: {
+            position: 'top',
+            horizontalAlign: 'right',
+            floating: true,
+            offsetY: -25,
+            offsetX: -5
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+</script>
 <?php $this->endSection(); ?>
